9461868b8a66978f2ed9a244ed84785ef158a473
vince <xiequan0759@163.com>
2017-09-05 20:26:30 +0800

    shopcart组件

---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="shopcart">
+    <div class="content">
+      <div class="content-left">
+        <div class="logo-wrapper">
+          <div class="logo">
+            <i class="icon-shopping_cart"></i>
+          </div>
+        </div>
+        <div class="price">￥{{ totalPrice }}</div>
+        <div class="desc">另需配送费￥{{ deliveryPrice }}元</div>
+      </div>
+      <div class="content-right">
+        <div class="pay">
+          ￥{{ minPrice }}元起送
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      selectFoods: {
+        type: Array,
+        default() {
+          return [
+            {
+              price: 10,
+              count: 1
+            }
+          ];
+        }
+      },
+      deliveryPrice: {
+        type: Number,
+        default: 0
+      },
+      minPrice: {
+        type: Number,
+        default: 0
+      }
+    },
+    computed: {
+      totalPrice() {
+        let total = 0;
+        this.selectFoods.forEach((food) => {
+          total += food.price * food.count;
+        });
+        return total;
+      },
+      totalCount() {
+        let count = 0;
+        this.selectFoods.forEach((food) => {
+          count += food.count;
+        });
+        return count;
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .shopcart
+    position: fixed
+    left: 0
+    bottom: 0
+    z-index: 50
+    width: 100%
+    height: 48px
+    .content
+      display: flex
+      font-size: 0
+      color: rgba(255, 255, 255, 0.4)
+      .content-left
+        flex: 1
+        background: #131d26
+        .logo-wrapper
+          display: inline-block
+          position: relative
+          top: -10px
+          margin: 0 12px
+          padding: 6px
+          vertical-align: top
+          width: 56px
+          height: 56px
+          box-sizing: border-box
+          background: #131d26
+          border-radius: 50%
+          .logo
+            text-align: center
+            border-radius: 50%
+            background: #2b343c
+            .icon-shopping_cart
+              line-height: 44px
+              font-size: 24px
+        .price
+          display: inline-block
+          vertical-align: top
+          box-sizing: border-box
+          margin-top: 12px
+          padding-right: 12px
+          line-height: 24px
+          border-right: 1px solid rgba(255, 255, 255, 0.1)
+          font-size: 16px
+          font-weight: 700
+        .desc
+          display: inline-block
+          margin:12px 0 0 12px
+          vertical-align: top
+          line-height: 24px
+          font-size: 12px
+      .content-right
+        flex: 0 0 105px
+        width: 105px
+        .pay
+          height: 48px
+          line-height: 48px
+          text-align: center
+          font-size: 16px
+          font-weight: 700
+          background-color: #2b333b
+</style>
