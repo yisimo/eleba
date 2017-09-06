fe8a58f029bc07f12f75820d8e9d36751a9fbc0e
vince <xiequan0759@163.com>
2017-09-06 14:57:50 +0800

    shopcart组件

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,16 +3,17 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{'highlight':totalCount>0}">
+            <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
           </div>
+          <div class="num" v-show="totalCount>0">{{ totalCount }}</div>
         </div>
-        <div class="price">￥{{ totalPrice }}</div>
+        <div class="price" :class="{'highlight':totalPrice>0}">￥{{ totalPrice }}</div>
         <div class="desc">另需配送费￥{{ deliveryPrice }}元</div>
       </div>
       <div class="content-right">
-        <div class="pay">
-          ￥{{ minPrice }}元起送
+        <div class="pay" :class="payClass">
+          {{ payDesc }}
         </div>
       </div>
     </div>
@@ -27,7 +28,7 @@
         default() {
           return [
             {
-              price: 10,
+              price: 20,
               count: 1
             }
           ];
@@ -56,6 +57,23 @@
           count += food.count;
         });
         return count;
+      },
+      payDesc() {
+        if (this.totalPrice === 0) {
+          return `￥${this.minPrice}元起送`;
+        } else if (this.totalPrice < this.minPrice) {
+          let diff = this.minPrice - this.totalPrice;
+          return `还差￥${diff}起送`;
+        } else {
+          return '去结算';
+        }
+      },
+      payClass() {
+        if (this.totalPrice < this.minPrice) {
+          return 'not-enough';
+        } else {
+          return 'enough';
+        }
       }
     }
   };
@@ -92,9 +110,27 @@
             text-align: center
             border-radius: 50%
             background: #2b343c
+            &.highlight
+              background: rgb(0, 160, 220)
             .icon-shopping_cart
               line-height: 44px
               font-size: 24px
+              &.highlight
+                color: #fff
+          .num
+            position: absolute
+            top: 0
+            right: 0
+            width: 24px
+            height: 16px
+            line-height: 16px
+            text-align: center
+            font-size: 9px
+            font-weight: 700
+            color: #fff
+            background: rgb(240, 20, 20)
+            border-radius: 16px
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
         .price
           display: inline-block
           vertical-align: top
@@ -105,6 +141,8 @@
           border-right: 1px solid rgba(255, 255, 255, 0.1)
           font-size: 16px
           font-weight: 700
+          &.highlight
+            color: #fff
         .desc
           display: inline-block
           margin:12px 0 0 12px
@@ -120,5 +158,9 @@
           text-align: center
           font-size: 16px
           font-weight: 700
-          background-color: #2b333b
+          &.not-enough
+            background-color: #2b333b
+          &.enough
+            background-color: #00b43c
+            color: #fff
 </style>
